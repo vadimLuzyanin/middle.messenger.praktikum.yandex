445abd5254ecc9a52a9f8f00ec62d33752e78bbe
Vadim Luzyanin <vadim.luzyanin@phkp.ru>
2021-11-18 14:11:19 +0300

    yarn dev

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require("path");
 const PORT = 3000;
 const app = express();
 
-app.use(express.static("dist"));
+app.use(express.static(path.join(__dirname, "dist")));
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "/dist/index.html"));
